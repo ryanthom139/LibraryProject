ec0b9fbda523aa9b9ab0bed14def2b0796bce7a0
Ryan Thompson [sc18rt] <sc18rt@comp-pc6166.leeds.ac.uk>
2019-02-22 16:07:41 +0000

    Backup V1.1

--- quadTree/buildTree.c
+++ quadTree/buildTree.c
@@ -37,3 +37,38 @@ void makeChildren( Node *parent ) {
 
   return;
 }
+
+void destroyTree(Node *head){
+  
+      int i;
+  
+      if( head->child[0] == NULL ){
+          free(head);
+      }
+      else
+      {
+          for ( i=0; i<4; i++ ) {
+              destroyTree(head->child[i]);
+              
+              }
+      }
+      free(head);
+      
+      return;
+  }
+
+  void growTree(Node *head){
+    
+        int i;
+        
+            if( head->child[0] == NULL ){
+                for ( i=0; i<4; i++ ) {   
+                    makeChildren(head->child[i]);
+                }
+            }
+            else {
+                for ( i=0; i<4; i++ ) {   
+                    growTree(head->child[i]);
+                }
+            }
+    }
--- quadTree/buildTree.h
+++ quadTree/buildTree.h
@@ -1,2 +1,4 @@
 Node *makeNode( double x, double y, int level );
 void makeChildren( Node *parent );
+void destroyTree(Node *head);
+void growTree(Node *head);
