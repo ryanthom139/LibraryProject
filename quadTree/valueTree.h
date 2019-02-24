093aeb9bbf1334e84598b620f8c522436fca6867
Ryan Thompson [sc18rt] <sc18rt@leeds.ac.uk>
2019-02-24 22:59:15 +0000

    Backup V1

---
+++ quadTree/valueTree.h
@@ -0,0 +1,6 @@
+#include "stdbool.h"
+
+double dataFunction( double x, double y, int choice );
+bool indicator( Node *node, double tolerance, int choice );
+void addChildren(Node *node, int choice, int tolerance);
+void monitorChild (int count);
