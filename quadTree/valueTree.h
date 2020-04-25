43dc8efe1a8978754aef2d77b72d7d1d035fbde3
Ryan Thompson <sc18rt@leeds.ac.uk>
2020-04-25 11:17:05 +0000

    Delete valueTree.h

--- quadTree/valueTree.h
+++
@@ -1,6 +0,0 @@
-#include "stdbool.h"
-
-double dataFunction( double x, double y, int choice );
-bool indicator( Node *node, double tolerance, int choice );
-int addChildren(Node *node, int choice, double tolerance);
-void monitorChild (Node *node, int choice, double tolerance);
